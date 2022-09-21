05fa4fc6df0bd13980bb1b64ad2f35544d4c213c
chenannchi <anchichenusa@gmail.com>
2022-09-20 23:03:05 -0400

    finish part 1

---
+++ data/skill-data.js
@@ -0,0 +1,10 @@
+const skills = [
+  {content:"C++",learned:true,level:6},
+  {content:"Python",learned:true,level:5},
+  {content:"Front-end",learned:true,level:3},
+  {content:"React",learned:false,level:0}
+]
+
+export { 
+	skills
+}
---
+++ routes/skills.js
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import { skills } from "../data/skill-data.js"
+import * as skillsCtrl from "../controllers/skills.js"
+
+const router = Router()
+
+/* GET users listing. */
+// router.get('/', function(req, res) {
+//   res.send('respond with a resource')
+// })
+
+router.get('/', skillsCtrl.index)
+
+export {
+  router
+}
